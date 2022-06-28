9949739673bffb8bef3353825b9b7f14519a420c
ArmanApoyan <arman.apoyan2@gmail.com>
2022-06-28 15:55:20 +0400

    28.06

--- src/store/Task/state.ts
+++ src/store/Task/state.ts
@@ -3,13 +3,6 @@ export const myState = {
   goals: [
     { id: 1, title: "Task 1", description: "Description 1", status: "To Do" },
     { id: 2, title: "Task 2", description: "Description 2", status: "Done" },
-    { id: 3, title: "Task 1", description: "Description 1", status: "To Do" },
-    { id: 4, title: "Task 1", description: "Description 1", status: "To Do" },
-    { id: 5, title: "Task 1", description: "Description 1", status: "To Do" },
-    { id: 6, title: "Task 1", description: "Description 1", status: "To Do" },
-    { id: 7, title: "Task 1", description: "Description 1", status: "To Do" },
-    { id: 8, title: "Task 1", description: "Description 1", status: "To Do" },
-    { id: 9, title: "Task 1", description: "Description 1", status: "To Do" },
   ],
   search:""
 };
