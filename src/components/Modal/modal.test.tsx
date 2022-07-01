b2356f4c80142efc011eda8c1114922f95ef1b28
ArmanApoyan <arman.apoyan2@gmail.com>
2022-07-01 18:01:01 +0400

    01.07 testing finished/no fails/formatted

--- src/components/Modal/modal.test.tsx
+++ src/components/Modal/modal.test.tsx
@@ -1,19 +1,46 @@
-import { fireEvent, render, screen } from "@testing-library/react"
-import Modal from "./"
+import { fireEvent, render, screen } from "@testing-library/react";
+import Modal from "./";
 
+describe("modal test", () => {
+  const close = jest.fn();
 
-describe("modal test",()=> {
-    const close = jest.fn()
+  it("modal rendering", () => {
+    const { container } = render(
+      <Modal isOpen={true} close={close}>
+        <p>asd</p>
+      </Modal>
+    );
+    expect(container).toMatchSnapshot();
+  });
 
-    it("modal rendering",()=>{
-        const {container} =  render(<Modal isOpen={true} close={close}><p>asd</p></Modal>)
-        expect(container).toMatchSnapshot()     
-    })  
-    
-    it("modal close",()=>{
-        render(<Modal isOpen={true} close={close}><p>asd</p></Modal>)
-        const btn = screen.getByTestId("btn")
-        fireEvent.click(btn)
-        expect(close).toBeCalled()
-    })
-})
+  it("modal close", () => {
+    render(
+      <Modal isOpen={true} close={close}>
+        <p>asd</p>
+      </Modal>
+    );
+    const btn = screen.getByTestId("btn");
+    fireEvent.click(btn);
+    expect(close).toBeCalled();
+  });
+
+  it("modal outside click", () => {
+    render(
+      <Modal isOpen={true} close={close}>
+        <p>asd</p>
+      </Modal>
+    );
+    fireEvent.mouseDown(document.body);
+    expect(close).toBeCalled();
+  });
+  
+  it("modal inside click", () => {
+    render(
+      <Modal isOpen={true} close={close}>
+        <p>asd</p>
+      </Modal>
+    );
+    fireEvent.mouseDown(screen.getByTestId("body"));
+    expect(close).not.toBeCalled();
+  });
+});
