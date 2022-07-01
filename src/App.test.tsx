3c34cd4d9c601d6a170129d525cd66cdb12c6cc4
ArmanApoyan <arman.apoyan2@gmail.com>
2022-07-01 16:39:48 +0400

    01.07 testing/no fail/formatted

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,18 @@
 import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { MemoryRouter } from "react-router-dom";
 import App from "./App";
+import { createTestStore } from "./testUtils/testUtils";
 
-test("renders learn react link", () => {
-  // render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  expect("linkElement").toEqual("linkElement");
+describe("App test", () => {
+  const store = createTestStore();
+
+  test("renders learn react link", () => {
+    const { container } = render(
+        <Provider store={store}>
+          <App />
+        </Provider>
+    );
+    expect(container).toMatchSnapshot()
+  });
 });
