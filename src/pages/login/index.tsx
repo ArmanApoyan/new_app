97d2b73034edf4f4a4bf3117cd8055d76e34c53c
ArmanApoyan <arman.apoyan2@gmail.com>
2022-07-04 11:53:52 +0400

    04.07 log/reg page validation

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -3,7 +3,7 @@ import { useForm } from "react-hook-form";
 import "./style.scss";
 
 const Log: React.FC = () => {
-  const [type, setType] = useState("reg");
+  const [type, setType] = useState("log");
   const {
     register,
     handleSubmit,
@@ -24,11 +24,12 @@ const Log: React.FC = () => {
           >
             <div className="reg_div">
               <label>Username</label>
-              <input
+              <input 
                 className="reg_input"
                 type="text"
-                {...register("username", { required: true })}
-              />
+                {...register("username", { required: true, minLength: 3 })}
+                />
+                {errors.username && <p className="error">Username is not correct</p>}
             </div>
             <div className="reg_div">
               <label>Email</label>
@@ -37,22 +38,26 @@ const Log: React.FC = () => {
                 type="email"
                 {...register("email", { required: true })}
               />
+              {errors.email && <p className="error">Email is not correct</p>}
+              
             </div>
             <div className="reg_div">
               <label>Password</label>
-              <input
+              <input 
                 className="reg_input"
                 type="password"
-                {...register("password", { required: true })}
+                {...register("password", { required: true, minLength: 6 })}
               />
+              {errors.password && <p className="error">Password is not correct</p>}
             </div>
             <div className="reg_div">
               <label>Confirm Password</label>
-              <input
+              <input 
                 className="reg_input"
                 type="password"
-                {...register("confirm", { required: true })}
+                {...register("confirm", { required: true, minLength: 6 })}
               />
+              {errors.confirm && <p className="error">Confirm Password is not correct</p>}
             </div>
             <button className="reg_btn">Sign Up</button>
           </form>
@@ -82,19 +87,23 @@ const Log: React.FC = () => {
           >
             <div className="reg_div">
               <label>Username</label>
-              <input
+              <input 
                 className="reg_input"
                 type="text"
-                {...register("username", { required: true })}
+                {...register("username", { required: true, minLength:3 })}
               />
+              {errors.username && <p className="error">Username is not correct</p>}
+
             </div>
             <div className="reg_div">
               <label>password</label>
-              <input
+              <input 
                 className="reg_input"
                 type="password"
-                {...register("password", { required: true })}
+                {...register("password", { required: true, minLength:6 })}
               />
+              {errors.password && <p className="error">Password is not correct</p>}
+
             </div>
             <button className="reg_btn">Log In</button>
           </form>
