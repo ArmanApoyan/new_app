a45092103bb7d2286e5512badf79b96b60c8e6d1
ArmanApoyan <arman.apoyan2@gmail.com>
2022-07-04 11:58:26 +0400

    04.01 minor changes

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -14,22 +14,23 @@ const Log: React.FC = () => {
     <>
       {type == "reg" && (
         <div className="reg">
-            <h1>Sign Up</h1>
+          <h1>Sign Up</h1>
           <form
             className="reg_form"
             onSubmit={handleSubmit((data) => {
               console.log(data);
-              reset()
-            })}
-          >
+              reset();
+            })}>
             <div className="reg_div">
               <label>Username</label>
-              <input 
+              <input
                 className="reg_input"
                 type="text"
                 {...register("username", { required: true, minLength: 3 })}
-                />
-                {errors.username && <p className="error">Username is not correct</p>}
+              />
+              {errors.username && (
+                <p className="error">Username is not correct</p>
+              )}
             </div>
             <div className="reg_div">
               <label>Email</label>
@@ -39,25 +40,28 @@ const Log: React.FC = () => {
                 {...register("email", { required: true })}
               />
               {errors.email && <p className="error">Email is not correct</p>}
-              
             </div>
             <div className="reg_div">
               <label>Password</label>
-              <input 
+              <input
                 className="reg_input"
                 type="password"
                 {...register("password", { required: true, minLength: 6 })}
               />
-              {errors.password && <p className="error">Password is not correct</p>}
+              {errors.password && (
+                <p className="error">Password is not correct</p>
+              )}
             </div>
             <div className="reg_div">
               <label>Confirm Password</label>
-              <input 
+              <input
                 className="reg_input"
                 type="password"
                 {...register("confirm", { required: true, minLength: 6 })}
               />
-              {errors.confirm && <p className="error">Confirm Password is not correct</p>}
+              {errors.confirm && (
+                <p className="error">Confirm Password is not correct</p>
+              )}
             </div>
             <button className="reg_btn">Sign Up</button>
           </form>
@@ -65,45 +69,45 @@ const Log: React.FC = () => {
             className="type"
             onClick={() => {
               setType("log");
-            }}
-          >
+            }}>
             Log In
           </p>
         </div>
       )}
       {type == "log" && (
         <div className="reg">
-            <h1>Log In</h1>
+          <h1>Log In</h1>
           <form
             className="reg_form"
             onSubmit={handleSubmit((data) => {
-                if(type=="log") {
-                    delete data.confirm
-                    delete data.email
-                }                
+              if (type == "log") {
+                delete data.confirm;
+                delete data.email;
+              }
               console.log(data);
-              reset()
-            })}
-          >
+              reset();
+            })}>
             <div className="reg_div">
               <label>Username</label>
-              <input 
+              <input
                 className="reg_input"
                 type="text"
-                {...register("username", { required: true, minLength:3 })}
+                {...register("username", { required: true, minLength: 3 })}
               />
-              {errors.username && <p className="error">Username is not correct</p>}
-
+              {errors.username && (
+                <p className="error">Username is not correct</p>
+              )}
             </div>
             <div className="reg_div">
               <label>password</label>
-              <input 
+              <input
                 className="reg_input"
                 type="password"
-                {...register("password", { required: true, minLength:6 })}
+                {...register("password", { required: true, minLength: 6 })}
               />
-              {errors.password && <p className="error">Password is not correct</p>}
-
+              {errors.password && (
+                <p className="error">Password is not correct</p>
+              )}
             </div>
             <button className="reg_btn">Log In</button>
           </form>
@@ -111,8 +115,7 @@ const Log: React.FC = () => {
             className="type"
             onClick={() => {
               setType("reg");
-            }}
-          >
+            }}>
             Sign Up
           </p>
         </div>
