b918da38c776c7a784aeb98c3aba4f15b9fddd7d
ArmanApoyan <arman.apoyan2@gmail.com>
2022-07-12 12:05:02 +0400

    12.07 refresh token fixed

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -7,17 +7,29 @@ Axios.defaults.baseURL = "http://localhost:5000/";
 Axios.defaults.timeout = 5000;
 
 export const axiosPost = async (url: string, data: any, token: string = "", userId:string="") => {
+  if(localStorage.token){
+    const result = await Axios.get("/refresh", {
+      headers: { Authorization: localStorage?.token, refresh:localStorage?.refresh },
+    }).then((res) => res.data); 
+    console.log(result.message);
+    if(result.newToken){
+      localStorage.token = result.newToken
+      localStorage.refresh = result.newRefresh
+    }
+  }
+  
+  
   return await Axios.post(
     url,
     { data },
     {
-      headers: { Authorization: token, userId:userId },
+      headers: { Authorization: localStorage.token, userId:userId },
     }
   ).then((res) => res.data);
 };
 
 export const axiosGet = async (url: string, token: string, userId:string="") => {
   return await Axios.get(url, {
-    headers: { Authorization: token, userId:userId },
+    headers: { Authorization: localStorage.token, userId:userId },
   }).then((res) => res.data);
 };
